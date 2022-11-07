a3ea8341cc2a74d58d0e0958d66f0e1825080b8f
TEMFACK DERICK <dericktemfack@gmail.com>
2022-11-07 18:54:42 +0100

    Filter the autocomplete field during the creation fo the quotation to only allow users to view his own client

--- models/res_partner.py
+++ models/res_partner.py
@@ -31,3 +31,26 @@ class PartnerExtend(models.Model):
             domain, fields, offset, limit, order)
 
         return res
+
+    @api.model
+    def _name_search(self, name, args=None, operator="ilike", limit=100, name_get_uid=None):
+        args = args or []
+
+        domain = []
+
+        # Get the connected user
+        connected_user = self.env.user
+
+        # Get all the sales teams
+        sales_teams = self.env['crm.team'].search([("active", '=', True)])
+
+        for team in sales_teams:
+
+            # Test if the connected user is a Team Lead
+            if connected_user in team.member_ids and connected_user == team.user_id:
+                domain += [("create_uid", 'in',
+                            [user.id for user in team.member_ids])]
+            elif connected_user in team.member_ids:
+                domain += [("create_uid", '=', connected_user.id)]
+
+        return self._search(domain+args, limit=limit, access_rights_uid=name_get_uid)
